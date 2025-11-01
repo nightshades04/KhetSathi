29b99c8a29cf23f536fa37d7b54f0f0e1771815b
nightshades04 <nightshades650@gmail.com>
2025-11-01 10:34:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -863,6 +863,3 @@ const Badge = mongoose.model('Badge', badgeSchema);
 app.get('/', (req, res) => {
     res.sendFile(public + '/index.html');
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
